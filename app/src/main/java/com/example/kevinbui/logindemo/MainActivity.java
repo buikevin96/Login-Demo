db446b8cf85849ac9b62bd03cac21cf058d7f6d3
buikevin96 <buikevin96@gmail.com>
2019-03-12 09:21:35 -0400

    Updated Main Actvity

--- app/src/main/java/com/example/kevinbui/logindemo/MainActivity.java
+++ app/src/main/java/com/example/kevinbui/logindemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kevinbui.logindemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,8 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText Name;
     private EditText Password;
-    private Button Info;
-    private TextView Login;
+    private Button Login;
+    private TextView Info;
+    private int counter = 5; // 5 attempts
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +25,38 @@ public class MainActivity extends AppCompatActivity {
 
        Name = (EditText)findViewById(R.id.etName);
        Password = (EditText)findViewById(R.id.etPassword);
-       Info = (Button)findViewById(R.id.btn);
-       Login = (TextView)findViewById(R.id.tvInfo);
+       Login = (Button)findViewById(R.id.btn);
+       Info = (TextView)findViewById(R.id.tvInfo);
+
+       Info.setText("No of attempts remaining: 5"); // Set info text to this
+
+
+       // Validate if Name and Password is the required fields
+       Login.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               validate(Name.getText().toString(), Password.getText().toString());
+           }
+       });
 
     }
 
     // Void because it is not returning anything
     private void validate(String userName, String userPassword){
-        if ((userName == "Admin") && (userPassword == "1234")) {
+        if ((userName.equals("Admin")) && (userPassword.equals("1234"))) {
 
             // To move onto next activity
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        } else {
+            counter--;
+
+            Info.setText("No of atttempts remaining: " + String.valueOf(counter)); // Converts int to sring
+
+            if(counter == 0){
+                // Disable button
+                Login.setEnabled(false);
+            }
         }
     }
 }
